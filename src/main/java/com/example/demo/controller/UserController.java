6fdf1be80ea8a05c06c38c9b2c3d9b2c7c7ee39b
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-12 17:16:47 +0300

    add authentication functionality

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -1,5 +1,11 @@
 package com.example.demo.controller;
 
+import com.example.demo.service.UserServiceImpl;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class UserController {
+    UserServiceImpl userService;
+
 
 }
--- src/main/java/com/example/demo/repository/UserRepository.java
+++ src/main/java/com/example/demo/repository/UserRepository.java
@@ -2,7 +2,12 @@ package com.example.demo.repository;
 
 import com.example.demo.model.User;
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.stereotype.Repository;
 
+import javax.transaction.Transactional;
+
+@Repository
+@Transactional
 public interface UserRepository extends PagingAndSortingRepository<User, Long> {
 
 }
