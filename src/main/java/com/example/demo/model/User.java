2e52bd569c20ae81ecdefb9113ea2fd382304c28
Ramzan Kurbanov <ramkurbanov10@yandex.ru>
2022-04-12 12:40:46 +0300

    add user entity

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -6,15 +6,14 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import java.time.LocalDate;
 
-@Data
 @Entity
-public class Task {
+@Data
+public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
-    private LocalDate date;
-    private String description;
-    private boolean done;
+
+    private String login;
+    private String password;
 }
---
+++ src/main/resources/db/migration/V20220412123036__task.sql
@@ -0,0 +1,4 @@
+CREATE TABLE task (
+                      id BIGSERIAL PRIMARY KEY,
+                      login TEXT NOT NULL,
+                      password TEXT NOT NULL );
